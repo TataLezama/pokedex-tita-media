c9185103b5a8776f8da9b8a0eab5cca765de34bf
Katherine <lezamakatherine@gmail.com>
2025-09-12 21:43:00 -0500

    Aplicación para publicar en Github pages

--- src/PokemonsApp.tsx
+++ src/PokemonsApp.tsx
@@ -1,10 +1,16 @@
-import { RouterProvider } from "react-router";
-import { appRouter } from "./router/app.router";
+import { HashRouter, Route } from "react-router";
+import { HomePage } from "./pages/home/HomePage";
+import { PokemonPage } from "./pages/pokemon/PokemonPage";
+import { FavoritesPage } from "./pages/favorites/FavoritesPage";
 
 export const PokemonsApp = () => {
   return (
     <>
-        <RouterProvider router= {appRouter}/>
+        <HashRouter>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pokemon/:id" element={<PokemonPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </HashRouter>
     </>
   )
 }
