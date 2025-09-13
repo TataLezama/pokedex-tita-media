5f27b48ec4a7bce91205846a8372fade354ee500
Katherine <lezamakatherine@gmail.com>
2025-09-12 21:22:14 -0500

    Cambio de url base

--- src/PokemonsApp.tsx
+++ src/PokemonsApp.tsx
@@ -1,10 +1,12 @@
-import { RouterProvider } from "react-router";
+import { BrowserRouter, RouterProvider } from "react-router";
 import { appRouter } from "./router/app.router";
 
 export const PokemonsApp = () => {
   return (
     <>
-        <RouterProvider router= {appRouter} />
+        <BrowserRouter basename="/pokedex-tita-media">
+            <RouterProvider router= {appRouter} />
+        </BrowserRouter>
     </>
   )
 }
