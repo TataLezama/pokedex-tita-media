1eab86df2dcae355f38a85377c92e755693e1980
Katherine <lezamakatherine@gmail.com>
2025-09-12 20:46:00 -0500

    Aplicación para publicar en Github pages

--- src/PokemonsApp.tsx
+++ src/PokemonsApp.tsx
@@ -1,10 +1,10 @@
-import { BrowserRouter, RouterProvider } from "react-router";
+import { RouterProvider } from "react-router";
 import { appRouter } from "./router/app.router";
 
 export const PokemonsApp = () => {
   return (
-    <BrowserRouter>
+    <>
         <RouterProvider router= {appRouter} />
-    </BrowserRouter>
+    </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pokemon-tita-media',
+  base: 'https://tatalezama.github.io/pokedex-tita-media/',
   plugins: [react()],
 })
